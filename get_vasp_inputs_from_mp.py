b760a630d06b5e01a8286cfba1102948b69062c9
wexler <wexler@wustl.edu>
2023-05-17 12:11:35 -0500

    feat: Enhance VASP input generation with added features and atomic perturbations
    This commit introduces enhanced functionality to search for and generate VASP inputs, now with additional features including atomic perturbations (rattling) and the saving of task IDs, ICSD IDs, and energy above hull for each structure.
    
    Key updates include:
    - `get_material_docs`: This function now retrieves relevant material documents from the Materials Project based on chemical system and possible species, specifically for alkali metals and oxygen.
    - `generate_vasp_inputs`: This function now not only generates VASP input sets but also introduces atomic perturbations to the structures (rattling), adjusts the cell size with a normal distribution, and writes task IDs, ICSD IDs, and energy above hull to separate text files.
    - `main` function: In addition to retrieving material documents and generating VASP inputs, this function now also retrieves conventional standard structures and additional data from each material document, and passes them to `generate_vasp_inputs`.
    
    The main function continues to leverage the MPRester API to fetch material documents for binary compounds containing alkali metals and oxygen. It then generates VASP input sets for each material using the MPScanRelaxSet settings, introduces atomic perturbations, and writes them to the specified directory. Additional material data (task IDs, ICSD IDs, and energy above hull) are also saved in this process.

--- get_vasp_inputs_from_mp.py
+++ get_vasp_inputs_from_mp.py
@@ -1,9 +1,15 @@
 import os
 
+from ase.io import write
+from emmet.core.provenance import Database
 from mp_api.client import MPRester
+from numpy.random import default_rng
 from pymatgen.core import Element, Species
+from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.io.vasp.sets import MPScanRelaxSet
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+
+rng = default_rng(42)
 
 alkali_metals = [str(Element.from_Z(i)) for i in range(1, 119) if Element.from_Z(i).is_alkali]
 
@@ -30,7 +36,7 @@ def get_material_docs(api_key, metals, nonmetal_species):
     return material_docs
 
 
-def generate_vasp_inputs(structure, directory):
+def generate_vasp_inputs(structure, directory, task_ids, icsd_ids, energy_above_hull):
     vasp_input_set = MPScanRelaxSet(
         structure,
         user_incar_settings={
@@ -42,6 +48,22 @@ def generate_vasp_inputs(structure, directory):
     )
     vasp_input_set.write_input(directory, include_cif=True)
 
+    # Rattle structure
+    os.rename(os.path.join(directory, "POSCAR"), os.path.join(directory, "POSCAR.higher_symmetry"))
+    atoms = AseAtomsAdaptor.get_atoms(structure)
+    atoms.rattle()
+    atoms.set_cell(atoms.cell + rng.normal(scale=0.001, size=atoms.cell.shape))
+    write(os.path.join(directory, "POSCAR"), atoms, format="vasp")
+
+    with open(os.path.join(directory, "task_ids.txt"), "w") as f:
+        f.write("\n".join(str(task_id) for task_id in task_ids) + "\n")
+
+    with open(os.path.join(directory, "icsd_ids.txt"), "w") as f:
+        f.write("\n".join(str(icsd_id) for icsd_id in icsd_ids) + "\n")
+
+    with open(os.path.join(directory, "energy_above_hull.txt"), "w") as f:
+        f.write(f"{energy_above_hull}\n")
+
 
 def main():
     api_key = os.getenv("MATERIALS_PROJECT_API_KEY")
@@ -52,9 +74,23 @@ def main():
     for material_doc in material_docs:
         structure = material_doc.structure
         conventional_standard_structure = SpacegroupAnalyzer(structure).get_conventional_standard_structure()
-        vasp_input_directory = os.path.join(directory, material_doc.formula_pretty)
-        generate_vasp_inputs(conventional_standard_structure, vasp_input_directory)
-        break
+
+        formula_pretty = material_doc.formula_pretty
+        symmetry_symbol = material_doc.symmetry.symbol.replace("/", "__")
+        material = f"{formula_pretty}_{symmetry_symbol}"
+
+        task_ids = [task_id.string for task_id in material_doc.task_ids]
+        icsd_ids = material_doc.database_IDs[Database.ICSD]
+        energy_above_hull = material_doc.energy_above_hull
+
+        vasp_input_directory = os.path.join(directory, material)
+        generate_vasp_inputs(
+            conventional_standard_structure,
+            vasp_input_directory,
+            task_ids,
+            icsd_ids,
+            energy_above_hull
+        )
 
 
 if __name__ == "__main__":
